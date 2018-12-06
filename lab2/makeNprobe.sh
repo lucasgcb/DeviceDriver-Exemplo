51a58344107194cb7bf28f303b7a95059fbcf9eb
Lucas Bandeira <lucascoliado@hotmail.com>
2018-12-06 11:21:32 -0200

    Adicionar ajustados scripts de QoL

--- lab2/makeNprobe.sh
+++ lab2/makeNprobe.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+if [ -z "$I2C_ATIVADO" ]
+then
+	echo "Probing I2C modules"
+	modprobe i2c-bcm2708
+	modprobe i2c-dev
+	lsmod | grep i2c
+	i2cdetect -y 1
+	export I2C_ATIVADO=1
+fi
+
+
 cd /home/pi/DeviceDriverLab2/
 rmmod batata_driver.ko
 rmmod industrialio
@@ -6,5 +18,4 @@ make clean
 make all
 modprobe industrialio
 insmod /home/pi/DeviceDriverLab2/batata_driver.ko
-lsmod
-dmesg | tail -n 20
+dmesg | tail -n 30
--- lab2/send2rpi.sh
+++ lab2/send2rpi.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+#ip_Do_Rpi= 172.16.158.68 //lab 2
+ip_Do_Rpi="192.168.0.24" #casa
+
 if [ "$#" -ne 1 ]
 then
-	scp batata_driver.c pi@172.16.158.68:~/DeviceDriverLab2/
-	ssh root@172.16.158.68 '/home/pi/DeviceDriverLab2/makeNprobe.sh'
+	scp batata_driver.c pi@$ip_Do_Rpi:~/DeviceDriverLab2/
+	ssh root@$ip_Do_Rpi  '/home/pi/DeviceDriverLab2/makeNprobe.sh'
 else
-	scp $1 pi@172.16.158.68:~/DeviceDriverLab2/
+	scp $1 pi@192.168.0.24:~/DeviceDriverLab2/
 fi
